58b0f6882e6a529ef944353f32e3c48d0f101a18
Ireneusz Piatkowski (SII SP. Z O. O. - POLAND) <Ireneusz.Piatkowski@motorolasolutions.com>
2024-12-16 16:33:15 +0100

    Włączenie cors dla **

--- src/main/java/pl/paraklet/sped_it/configuration/WebConfig.java
+++ src/main/java/pl/paraklet/sped_it/configuration/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/gieldaladunkow")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173") // frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
